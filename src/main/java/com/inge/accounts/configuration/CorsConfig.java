94e76b71a34e9bd4f3992f6509107c579b04f90d
GeovaniPassos <geovanipassos95@gmail.com>
2026-01-14 11:16:59 -0300

    Ajuste no cors para liberar o acesso a api

--- src/main/java/com/inge/accounts/configuration/CorsConfig.java
+++ src/main/java/com/inge/accounts/configuration/CorsConfig.java
@@ -14,8 +14,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+                        .allowedOrigins("http://127.0.0.1:5500",
+                                "https://geovanipassos.github.io/acconts-painel"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
