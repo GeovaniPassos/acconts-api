fd355ed8e59ed2e9bfcf977206fae1e9367fbba0
GeovaniPassos <geovanipassos95@outlook.com>
2026-03-24 13:12:16 -0300

    Configurado as variaveis de ambientes

--- src/main/java/com/inge/accounts/auth/security/JwtService.java
+++ src/main/java/com/inge/accounts/auth/security/JwtService.java
@@ -3,6 +3,8 @@ package com.inge.accounts.auth.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -10,8 +12,11 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    //Chave temporária para teste em desenvolvimento
-    private final String SECRET = "ca3c62006e32e785d37cba3d855d2210aa697d53a37c7e7590efc34fabf3f390";
+    private final String SECRET;
+
+    public JwtService(@Value("${jwt.secret}") String secret) {
+        SECRET = secret;
+    }
 
     public String generateToken(String username) {
 
